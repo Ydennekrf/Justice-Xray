371a093c3a02aafbb4f9b3f105c8fe58127c978c
ydennekrf <ydennekrf@gmail.com>
2022-06-24 11:07:32 -0400

    added comment.js model

---
+++ models/comment.js
@@ -0,0 +1,48 @@
+const { INTEGER } = require('sequelize');
+const { Model, DataTypes } = require('sequelize');
+const { DefaultSerializer } = require('v8');
+const sequelize = require('../config/connections');
+
+class Comment extends Model {}
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        },
+        post_id: {
+            type: DataTypes:INTEGER,
+            allowNull: false,
+            referenses: {
+                model: 'post',
+                key: 'id'
+            }
+        },
+        comment_txt: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [2,255]
+            }
+        }
+    },
+    {
+        
+            sequelize,
+            freezeTableName: true,
+            underscored: true,
+            modelName: 'comment'
+        });
+
+module.exports = Comment;
