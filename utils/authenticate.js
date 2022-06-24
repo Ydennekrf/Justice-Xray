d9322d0f4be9a91feaffcd01d158c65e6672e7c7
ydennekrf <ydennekrf@gmail.com>
2022-06-24 08:21:43 -0400

    initial commit file structure and db set up

---
+++ utils/authenticate.js
@@ -0,0 +1,11 @@
+//taken from activity 23
+
+const withAuth = (req, res, next) => {
+    if (!req.session.logged_in) {
+      res.redirect('/login');
+    } else {
+      next();
+    }
+  };
+  
+  module.exports = withAuth;
