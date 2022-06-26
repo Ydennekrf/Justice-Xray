e52318a5a47cdea91638987a5b89cf74c7d9b176
ydennekrf <ydennekrf@gmail.com>
2022-06-26 09:40:08 -0400

    created homeroute.js controllers set up some handlebars

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const sequelize = require('../config/connections');
 const { Post, User, Comment } = require('../models');
 
-
+// renders home page
 router.get('/', (req, res) => {
     console.log(req.body)
 
@@ -23,8 +23,10 @@ router.get('/', (req, res) => {
     ]
     })
     .then(dbpost => {
-        // const post = dbpost.map(post => post.get({ plain: true }));
-        // render home here
+        const post = dbpost.map(post => post.get({ plain: true }));
+        res.render('home', {
+            post, loggedIn: req.session.loggedIn
+        });
        
     })
     .catch(err => {
@@ -32,6 +34,55 @@ router.get('/', (req, res) => {
         res.status(500).json(err);
     })
 });
+// takes you to login page
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    res.render('login');
+});
+// takes you to new user page
+router.get('/newUser', (req, res) => {
+    if(req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    res.render('createUser');
+});
+
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id', 'title', 'post_txt'],
+        include: [{
+            model: Comment,
+            attributes: ['id', 'comment_txt', 'post_id', 'user_id'],
+            include: {
+                model: User,
+                attributes: ['username']
+            }
+        },
+    {
+        model: User,
+        attributes: ['username']
+    }]
+    })
+    .then(dbPost => {
+        if (dbPost === false) {
+            res.status(404).json({ message: 'id not found'});
+            return;
+        }
+        res.render('newPost', {
+            post, loggedIn: req.session.loggedIn
+        });
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+})
 
 module.exports = router;
 
