9cef7d3fe2b40d04db5033976cc1682d23418b5f
ydennekrf <ydennekrf@gmail.com>
2022-06-27 07:39:30 -0400

    finished comment.js controllers/api

--- controllers/api/comment.js
+++ controllers/api/comment.js
@@ -10,7 +10,39 @@ router.get('/', (req,res) => {
         res.status(500).json(err);
     })
 });
-
+//adds new post to db
 router.post('/', auth, (req,res) => {
-    
-})
+    if (req.session) {
+        Comment.create({
+            comment_txt: req.body.comment_txt,
+            post_id: req.body.post_id,
+            user_id: req.session.user_id
+        })
+        .then(dbcomment => res.json(dbcomment))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        })
+    }
+});
+//deletes post from db based on whihc user is logged in
+router.delete('/:id', auth, (req,res) => {
+    Comment.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbcomment => {
+        if (dbcomment === false) {
+            res.status(404).json({ message: 'id not found' });
+            return;
+        }
+        res.json(dbcomment);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+});
+
+module.exports = router;
